670e3c72d0f74a2cf0634a22d3b9979ad12d291e
cinthia3301andrad <cinthiaadm15@gmail.com>
2019-06-06 19:37:59 -0300

    Add finaliz forca

--- forca.py
+++ forca.py
@@ -1,6 +1,6 @@
 import random
-def escolher_palavras():
-    arquivo = open('palavras.txt', 'r')
+def escolher_palavras(arquivos):
+    arquivo = open(arquivos, 'r')
     lista_palavras = []
     for linha in arquivo.readlines():
         todas_palavras = linha.strip()
@@ -22,24 +22,28 @@ def jogada(escolha):
     while quant_chutes <= 6:
         letra = input('Digite uma letra: ')
         letra_usada.append(letra)
-        if letra in escolha: #Condição da letra estar na minha palavra random.
-                if letra_usada.count(letra) == 1: #E o usuário não ter digitado ela ainda
+        if letra in escolha: #1° Condição da letra estar na minha palavra random.
+                if letra_usada.count(letra) == 1:#2° condição, o usuário ter digitado a letra pela primeira vez
                         print('Você acertou!')
                         for i in range(len(escolha)):
                                 if letra == escolha[i]:#Verificar onde a letra que o usuário digitou é igual a letra da palavra random.
-                                        resposta[i] = letra #Substitui o traço pela letra digitada
-                else:#Caso o usuário já tenha digitado a letra
+                                        palavra[i] = letra #Substitui o traço pela letra digitada
+                                        aux += 1
+                        if aux == len(escolha):
+                                print('Você ganhou!')
+                                print(palavra)
+                                break
+                else:#Caso o usuário já tenha digitado a letra, ou seja, mais de 1 vez
                         print('Letra já usada!')
-                print(resposta)
-        else:
+                print(palavra)
+        else:#Se a letra não estiver na palavra random.
                 quant_chutes += 1
-                print(quant_chutes)
-                print('Você errou!')
-                print(resposta)
+                print('Você errou! Você tem mais', 6-quant_chutes, 'chances de errar')
                 if quant_chutes == 6:
                         print('Suas chances acabaram!')
                         print('A palavra era: ', escolha)
                         break
+                        
 
 
 
@@ -47,6 +51,5 @@ print('='*30)
 print('         JOGO DA FORCA')
 print('---- Você terá 6 chances! ----')
 print('='*30)
-
-palavra = escolher_palavras()
+palavra = escolher_palavras('gabriel.txt')
 jogada(palavra)
